a50e44efc3071d2135873be10a03a72f803f6cbf
webdev-ashishk <webdev.ashishk@gmail.com>
2023-11-29 22:25:23 +0530

    footer styled

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,9 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div
-      // style={{ position: "relative", bottom: 0, width: "98%" }}
-      className="h-20 border-2 m-2 "
-    >
-      <div className="flex justify-center">
-        <h1>SUMAN MALAKAR @{currentYear}</h1>
+    <div className="m-2 mt-9">
+      <div className="text-blue-500  px-2 py-2 border-t-2 w-full text-center">
+        SUMAN MALAKAR @{currentYear}
       </div>
     </div>
   );
