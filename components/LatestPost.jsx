2e5fa5670092a82aabafb38d8152593a089501f6
webdev-ashishk <webdev.ashishk@gmail.com>
2023-11-30 15:33:32 +0530

    styled fatched data

--- components/LatestPost.jsx
+++ components/LatestPost.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function getLatestPostData() {
   const res = await fetch("http://localhost:3000/api/posts");
   if (!res.ok) {
@@ -10,16 +12,20 @@ const LatestPost = async () => {
   const posts = await getLatestPostData();
   console.log(posts);
   return (
-    <main>
+    <main className="flex flex-wrap justify-center items-center">
       {posts.map((post) => (
-        <div key={post.id}>
-          {/* <Image
+        <div
+          key={post.id}
+          className="hover:scale-y-105 rounded-lg  m-4 w-[200px] pt-1 border-2 hover:bg-gradient-to-r from-purple-400"
+        >
+          <Image
             src={post.image}
             width={200}
             height={200}
             alt="post-image"
-          ></Image> */}
+          ></Image>
           <h1>{post.title}</h1>
+          <p>{post.postDate}</p>
         </div>
       ))}
     </main>
